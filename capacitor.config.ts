f585048aed3734034996b91d116b2f44cfe44d88
Niklas Wimmer <nwimmer@student.tgm.ac.at>
2023-03-03 08:03:54 +0100

    Run ionic start with list template
    Signed-off-by: Niklas Wimmer <nwimmer@student.tgm.ac.at>

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'at.ac.tgm.shopilist',
+  appName: 'Shopilist',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
