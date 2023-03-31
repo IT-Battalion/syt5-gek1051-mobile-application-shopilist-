d6e45e437b4c6c7d6eb5bc1ea0a27b30e1ef8aac
Niklas Wimmer <nwimmer@student.tgm.ac.at>
2023-03-31 08:57:14 +0200

    Add input field to Add view
    Signed-off-by: Niklas Wimmer <nwimmer@student.tgm.ac.at>

--- shopilist/src/views/AddItemsView.vue
+++ shopilist/src/views/AddItemsView.vue
@@ -1,6 +1,32 @@
 <script setup lang="ts">
-import { IonPage, IonHeader, IonTitle, IonButtons, IonBackButton, IonToolbar } from "@ionic/vue"
+import {
+  IonPage,
+  IonHeader,
+  IonTitle,
+  IonButtons,
+  IonBackButton,
+  IonToolbar,
+  IonContent,
+  IonItem,
+  IonSearchbar,
+  IonLabel,
+  IonIcon,
+  onIonViewDidEnter,
+} from "@ionic/vue"
+import { addCircle } from "ionicons/icons"
+import { ref } from "vue";
 
+const inputEl = ref<InstanceType<typeof IonSearchbar> | null>(null);
+const input = ref<string>();
+
+function addShopilistItem(name: string) {
+  input.value = "";
+  console.log("adding new item with name " + name);
+}
+
+onIonViewDidEnter(() => {
+  inputEl.value?.$el.setFocus().then(() => {/* ensure the returned Promise is driven to completion */});
+});
 </script>
 
 <template>
@@ -13,5 +39,17 @@ import { IonPage, IonHeader, IonTitle, IonButtons, IonBackButton, IonToolbar } f
         <ion-title>Add Items</ion-title>
       </ion-toolbar>
     </ion-header>
+    <ion-content>
+      <ion-item>
+        <ion-searchbar ref="inputEl" v-model="input"
+          @keyup.enter="input != undefined ? addShopilistItem(input) : ''"></ion-searchbar>
+      </ion-item>
+      <ion-item v-if="input != undefined && input.length != 0" @click="addShopilistItem(input!)">
+        <ion-icon :icon="addCircle"></ion-icon>
+        <ion-label class="ion-text-wrap">
+          {{ input }}
+        </ion-label>
+      </ion-item>
+    </ion-content>
   </ion-page>
 </template>
